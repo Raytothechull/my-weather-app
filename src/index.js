6e760b647300cdf18e4409caae1254ee769d369b
Rachel <rachel.ruiz.2011@gmail.com>
2024-05-19 11:28:53 -0700

    Updated JS and minor HTML

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input")
-  let cityElement = document.querySelector("#city")
-  cityElement.innerHTML = searchInput.value;
+function updateWeather(response) {
+let temperatureElement = document.querySelector("#temperature")
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function searchCity(city) {
+ let apiKey = "fa3bt27161bo04aaa147a05bfae3f253";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+axios.get(apiUrl).then(updateWeather);
+}
 
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  
+  searchCity(searchInput.value);
+}
 
-let searchBoxElement = document.querySelector("#search-form");
-searchBoxElement.addEventListener("submit", handleSearchSubmit)
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Las Vegas");
