3da03c69e28b79c8b638d3210e27f5624c554dd7
Rachel <rachel.ruiz.2011@gmail.com>
2024-05-19 09:09:36 -0700

    added basic search engine on JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("hi hi")
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input")
+  let cityElement = document.querySelector("#city")
+  cityElement.innerHTML = searchInput.value;
+}
+
+
+
+let searchBoxElement = document.querySelector("#search-form");
+searchBoxElement.addEventListener("submit", handleSearchSubmit)
