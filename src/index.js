859d59c9b4390aff99f3e2bd7c3563238ca2a4f2
Rachel <rachel.ruiz.2011@gmail.com>
2024-05-23 12:07:43 -0700

    Updated HTML and added icons to work well on Javascript

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed")
 let timeElement = document.querySelector("#time");
 let date = new Date(response.data.time * 1000);
-console.log (response.data);
+let iconElement = document.querySelector("#icon");
 
 cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
@@ -15,7 +15,7 @@ temperatureElement.innerHTML = Math.round(temperature);
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
 timeElement.innerHTML = formatDate(date);
-}
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;}
 
 function formatDate(date) {
   
@@ -38,6 +38,7 @@ if (minutes < 10) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
 
 function searchCity(city) {
  let apiKey = "fa3bt27161bo04aaa147a05bfae3f253";
