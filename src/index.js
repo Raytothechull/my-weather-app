90ff4df0e2706c1bdf137c8111a38e1cf73391ce
Rachel <rachel.ruiz.2011@gmail.com>
2024-05-30 18:13:19 -0700

    Updated time in Javascript

--- src/index.js
+++ src/index.js
@@ -1,59 +1,70 @@
 function updateWeather(response) {
-let temperatureElement = document.querySelector("#temperature")
-let temperature = response.data.temperature.current;
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed")
-let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
-let iconElement = document.querySelector("#icon");
-
-cityElement.innerHTML = response.data.city;
-descriptionElement.innerHTML = response.data.condition.description;
-temperatureElement.innerHTML = Math.round(temperature);
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
-timeElement.innerHTML = formatDate(date);
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;}
-
-function formatDate(date) {
+  let temperatureElement = document.querySelector("#temperature")
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed")
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
   
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let days = [
-    "Sunday", 
-    "Monday",
-    "Tuesday", 
-    "Wednesday", 
-    "Thursday", 
-    "Friday", 
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-
-if (minutes < 10) {
-  minutes = `0${minutes}`
-}
-
-  return `${day} ${hours}:${minutes}`;
-}
-
-
-function searchCity(city) {
- let apiKey = "fa3bt27161bo04aaa147a05bfae3f253";
- let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
-axios.get(apiUrl).then(updateWeather);
-}
-
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;}
   
-  searchCity(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Las Vegas");
+  function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday", 
+      "Monday",
+      "Tuesday", 
+      "Wednesday", 
+      "Thursday", 
+      "Friday", 
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    let amPm = 'AM'
+    if(hours >= 12) {
+      amPm = 'PM'
+    }
+  
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    if(hours < 10) {
+      hours = `0${hours}`
+    }
+  
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`
+    }
+  
+    return `${day} ${hours}:${minutes} ${amPm}`;
+  }
+  
+  
+  function searchCity(city) {
+   let apiKey = "fa3bt27161bo04aaa147a05bfae3f253";
+   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+  axios.get(apiUrl).then(updateWeather);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    
+    searchCity(searchInput.value);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Las Vegas");
